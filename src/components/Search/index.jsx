2ba43b38c1e1baf09acbfe8f690fb12ece0ed914
Annie <anniehartmann97@gmail.com>
2023-02-03 23:21:16 +0300

    :tada: Done

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.css";
+
+const Search = ({ setSearch }) => {
+  return (
+    <input
+      input
+      type={"text"}
+      className={styles.search}
+      placeholder={"search"}
+      onChange={({ currentTarget: input }) => setSearch(input.value)}
+    />
+  );
+};
+
+export default Search;
